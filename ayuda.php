c1df0dfcc0502352decf65d4688b1ad05f217102
WiloCh <69049094+WiloCh@users.noreply.github.com>
2020-08-12 13:22:17 -0500

    Cambios en Ayuda- pruebaLlenado

--- ayuda.php
+++ ayuda.php
@@ -8,6 +8,19 @@
 <body>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam repudiandae blanditiis dolore maxime temporibus quasi nam cupiditate nihil, placeat iusto omnis? Illo doloribus odio itaque beatae ut, suscipit sint nobis?</p>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque aperiam incidunt nam commodi veritatis, eos harum inventore expedita consectetur itaque maiores suscipit? Enim illum aut illo quia laboriosam ipsa dicta?</p>
-    
+    <table>
+        <tr>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+        </tr>
+        <tr>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>    
+        </tr>
+    </table>
 </body>
 </html>
